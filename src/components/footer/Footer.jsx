e73733e869e8fc0c488bfef00d3ae5910fa296af
admin <admin@example.com>
2026-07-13 21:32:43 -0300

    Fix footer image paths for Vercel

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,15 +6,15 @@ function Footer() {
             <div className={style.footerContent}>
                 <div className={style.teamIcons}>
                     <div className={style.teamMember}>
-                        <img src="./public/images/MESSI.png" alt="Integrante 1" className={style.teamIcon} />
+                        <img src="/images/MESSI.png" alt="Integrante 1" className={style.teamIcon} />
                         <p className={style.teamDescription}>Capitán del equipo</p>
                     </div>
                     <div className={style.teamMember}>
-                        <img src="./public/images/musk.png" alt="Integrante 2" className={style.teamIcon} />
+                        <img src="/images/musk.png" alt="Integrante 2" className={style.teamIcon} />
                         <p className={style.teamDescription}>Tech Leader</p>
                     </div>
                     <div className={style.teamMember}>
-                        <img src="./public/images/jenseng.png" alt="Integrante 3" className={style.teamIcon} />
+                        <img src="/images/jenseng.png" alt="Integrante 3" className={style.teamIcon} />
                         <p className={style.teamDescription}>CEO cooperativo</p>
                     </div>
                 </div>
